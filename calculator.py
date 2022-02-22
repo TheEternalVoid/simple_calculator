f114cb452b8ca4eae93eed2790236fc77dd3cec7
Larry Holland <larry.holland4779@hotmail.com>
2022-02-22 15:55:43 -0500

    Created */- button and functionality

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from distutils import command
+import math
 from sqlite3 import Row
 from tkinter import *
 
@@ -21,13 +22,50 @@ def button_clear():
 def button_add():
     first_number = field.get()
     global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    field.delete(0, END)
+
+def button_subtract():
+    first_number = field.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    field.delete(0, END)
+
+def button_multiply():
+    first_number = field.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    field.delete(0, END)   
+
+def button_divide():
+    first_number = field.get()
+    global f_num
+    global math
+    math = "division"
     f_num = int(first_number)
     field.delete(0, END)
 
 def button_equal():
     second_number = field.get()
     field.delete(0, END)
-    field.insert(0, f_num + int(second_number))
+    
+    if math == "addition":
+        field.insert(0, f_num + int(second_number))
+    
+    if math == "subtraction":
+        field.insert(0, f_num - int(second_number))
+    
+    if math == "multiplication":
+        field.insert(0, f_num * int(second_number))
+    
+    if math == "division":
+        field.insert(0, f_num / int(second_number))
 
 #Define buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -42,8 +80,12 @@ button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=41, pady=20, command=button_divide)
 button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+
 
 #Place buttons on to the window
 button_1.grid(row=3, column=0)
@@ -63,5 +105,9 @@ button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 root.mainloop()
